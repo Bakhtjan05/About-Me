3ac3d63be6d3f0858b17c8ec203f302bd933b9c9
Bakhtjan05 <116889745+Bakhtjan05@users.noreply.github.com>
2023-03-27 16:22:38 +0500

    upgrade

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const home = document.getElementById('home')
 const aboutMe = document.getElementById('aboutMe')
 const portfolio = document.getElementById('portfolioLink')
 const contact = document.getElementById('contact')
+const title = document.getElementById('title')
+const titleInfo = ['Landing Foodieland', 'Sprav.uz']
+const portfolioImg = document.getElementById('portfolio-img')
+const rightSide = document.getElementById('rightSide')
+const leftSide = document.getElementById('leftSide')
+const imgs = ['./assets/images/portfolio-1.png', './assets/images/portfolio-2.png'];
+const link = document.getElementById('link')
+const linkInfo = ['https://foodieland-portfolio.netlify.app/', 'https://bakhtjan05.github.io/sprav.uz/']
 
-
-
+let active = 0;
 
 
 window.addEventListener('scroll', () => {
@@ -27,4 +34,23 @@ const openSidebar = () => {
 
 const closeSidebar = () => {
     sidebar.style.display = 'none'
+}
+
+const toRight = () => {
+    active++;
+    if(active >= imgs.length) active = 0
+    portfolioImg.src = imgs[active]
+    title.innerHTML = titleInfo[active]
+    link.href = linkInfo[active]
+
+}
+
+const toLeft = () => {
+    active--;
+    if(active = -1) active = 0
+    portfolioImg.src = imgs[active]
+    title.innerHTML = titleInfo[active]
+    link.href = linkInfo[active]
+
+
 }
